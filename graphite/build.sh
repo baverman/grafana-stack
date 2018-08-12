82390f7e322e572d99890570efa1c70ec74e13bc
Anton Bobrov <baverman@gmail.com>
2018-08-12 23:22:41 +0300

    graphite 1.1.3

--- graphite/build.sh
+++ graphite/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 name=${1:-baverman/graphite}
-version=${2:-1.1.1}
+version=${2:-1.1.3}
 tag=${3:-$version-1}
 
 docker build --build-arg=GRAPHITE_VERSION=$version -t $name:$tag -t $name:latest graphite
