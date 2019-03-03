39087baa10d60300bde7441f0f8ae8abbf431f38
Anton Bobrov <baverman@gmail.com>
2019-03-03 19:10:39 +0300

    grafana 5.4.3

--- grafana/build.sh
+++ grafana/build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+cd $(dirname $0)
 name=${1:-baverman/grafana}
-version=${2:-5.2.2}
-hsh=${3:-4267618981b93135644dd88f6f1cd2f1aa184b96ff845c8164c38dcb6e3fd777}
+version=${2:-5.4.3}
+hsh=${3:-c4d2a4723cfd7e5943e42786548ea2ccbc08cd1be80b5f447ef7309d9bd91527}
 tag=${4:-$version}
 
 docker build --build-arg=GRAFANA_VERSION=$version --build-arg=GRAFANA_CHECKSUM=$hsh \
-             -t $name:$tag -t $name:latest grafana
+             -t $name:$tag -t $name:latest .
+
+if [ "$PUSH" ]; then
+    docker push $name:$tag
+    docker push $name:latest
+fi
