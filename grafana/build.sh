912c60c6a2e150d6e786d0675ca2059c3b6d1698
Anton Bobrov <baverman@gmail.com>
2019-06-03 07:28:48 +0300

    grafana 6.2.1

--- grafana/build.sh
+++ grafana/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd $(dirname $0)
 name=${1:-baverman/grafana}
-version=${2:-5.4.3}
-hsh=${3:-c4d2a4723cfd7e5943e42786548ea2ccbc08cd1be80b5f447ef7309d9bd91527}
+version=${2:-6.2.1}
+hsh=${3:-b3ae6ed9874a8945e5a3c66ff94ef0b7689fa497928f448a0ba0149470d8086d}
 tag=${4:-$version}
 
 docker build --build-arg=GRAFANA_VERSION=$version --build-arg=GRAFANA_CHECKSUM=$hsh \
