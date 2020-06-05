50372d043816a6a6a497db57d3ed0b8865d05a99
Anton Bobrov <baverman@gmail.com>
2020-06-05 15:20:43 +0300

    grafana 6.7.4

--- grafana/build.sh
+++ grafana/build.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 cd $(dirname $0)
 name=${1:-baverman/grafana}
-version=${2:-6.2.1}
-hsh=${3:-b3ae6ed9874a8945e5a3c66ff94ef0b7689fa497928f448a0ba0149470d8086d}
+version=${2:-6.7.4}
+hsh=${3:-8e660a03b672059f8875e4ef602d3f85250d82f7f55327cedcc95e9bbd08051c}
 tag=${4:-$version}
 
-docker build --build-arg=GRAFANA_VERSION=$version --build-arg=GRAFANA_CHECKSUM=$hsh \
+if [ -n "$PROXY" ]; then
+    proxy_opts="--build-arg=http_proxy=$PROXY --build-arg=HTTP_PROXY=$PROXY"
+fi
+
+docker build --network=host $proxy_opts \
+             --build-arg=GRAFANA_VERSION=$version \
+             --build-arg=GRAFANA_CHECKSUM=$hsh \
              -t $name:$tag -t $name:latest .
 
-if [ "$PUSH" ]; then
+if [ -n "$PUSH" ]; then
     docker push $name:$tag
     docker push $name:latest
 fi
